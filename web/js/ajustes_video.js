0646d616adda886a85bf0cb7646496cca1ed8191
Henrique Pessolato <pessolatohenrique@gmail.com>
2017-08-20 16:26:55 -0300

    Ajuste de video

--- web/js/ajustes_video.js
+++ web/js/ajustes_video.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	var isCellphone = window.matchMedia('screen and (max-width: 626px)').matches;
-	$("iframe").attr("width",380);
-	$("iframe").attr("height",215);
+	if(isCellphone){
+		$("iframe").attr("width",380);
+		$("iframe").attr("height",215);
+	}
 });
